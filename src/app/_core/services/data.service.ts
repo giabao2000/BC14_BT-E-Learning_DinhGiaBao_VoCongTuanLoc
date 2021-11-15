b368bbcade2b255d13dcbeb540390c9c2a33a567
giabao2000 <dinhgiabao0506@gmail.com>
2021-11-15 11:09:19 +0700

    upload source code angular routing buoi 5

---
+++ src/app/_core/services/data.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from "rxjs"
+import { tap, catchError } from "rxjs/operators"
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  // Observable: tương tự như promise(), nhưng có thể hủy gọi api dc
+  getListCourse(): Observable<any> {
+    const url = 'https://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhSachKhoaHoc?MaNhom=GP01';
+    const observable = this.http.get(url).pipe(
+      // Success
+      tap(() => {}),
+      // Failed
+      catchError((error: any) => {
+        return this.handleError(error);
+      })
+    );
+    
+    return observable;
+  }
+
+  handleError(error: any) {
+    switch (error.status) {
+      case 300:
+        break;
+      
+      case 400:
+        break;
+      
+      case 500:
+        break;
+      
+      default:
+        break;
+    }
+
+    return throwError(error);
+  }
+}
--- src/app/pages/home-template/list-course/list-course.component.ts
+++ src/app/pages/home-template/list-course/list-course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/_core/services/data.service';
 
 @Component({
   selector: 'app-list-course',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-course.component.scss']
 })
 export class ListCourseComponent implements OnInit {
+  listCourse: any;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
+  // tương đương componentDidMount() 
+  // đầu tiên vào list-course thì hàm này sẽ chạy đầu tiên và chỉ chạy 1 lần
   ngOnInit(): void {
+    this.getCourse();
+  }
+
+  getCourse() {
+    // muốn lấy data trong getListCourse() thì phải sử dụng subscribe()
+    this.data.getListCourse().subscribe((result: any) => {
+      console.log(result);
+      this.listCourse = result;
+    });
   }
 
 }
