1eb569d0d5cf047fb3f601cb50f5763b209821b0
giabao2000 <dinhgiabao0506@gmail.com>
2021-11-26 16:42:04 +0700

    upload source code project  HomeTemplate E-Learning Third

--- src/app/pages/home-template/detail-user/registered-course/registered-course.component.ts
+++ src/app/pages/home-template/detail-user/registered-course/registered-course.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { DataService } from '@services/data.service';
 
 @Component({
